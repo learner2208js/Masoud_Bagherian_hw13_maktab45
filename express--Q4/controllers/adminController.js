1729cb899534a40c78eedb48a09773f04ef294bc
Pouria Baqerian <pouriacss.69@gmail.com>
2021-02-22 17:53:41 +0330

    Q4 controllers added

--- express--Q4/controllers/adminController.js
+++ express--Q4/controllers/adminController.js
@@ -1,12 +1,3 @@
-// const fs = require('fs');
-// const path = require('path');
-// let users;
-// try {
-//   users = JSON.parse(fs.readFileSync(path.join(__dirname, './../users.json')));
-// } catch (error) {
-//   console.log(error.message);
-//   users = [];
-// }
 const users = require('./../tools/downloadUsers');
 
 const getAllUsers = (req, res) => {
